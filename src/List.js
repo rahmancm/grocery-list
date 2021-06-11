99bf215ce1fc4be2d72517d2327d6724aaff3593
Rahman CM <72619650+rahmancm@users.noreply.github.com>
2021-06-11 18:24:39 +0530

    Update List.js

--- src/List.js
+++ src/List.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
-const List = ({ list, removeItem, editItem }) => {
+const List = ({ list, removeItem, editItem ,setStatus ,status}) => {
   return (
     <div className="grocery-list">
       {list.map((item) => {
         const { id, title } = item;
         return (
           <article key={id} className="grocery-item">
-            <p className="title">{title}</p>
+            <p  className={status ? "completed":"title"}>{title}</p>
             <div className="btn-container">
+              <label class="switch">
+              <input type="checkbox" onClick={()=> setStatus(true)}>
+             <span class="slider round"></span>
+              </label>
               <button
                 onClick={() => editItem(id)}
                 type="button"
